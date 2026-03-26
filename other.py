597d548d5fedd610a70c8c0e1ca273de7431b19f
Morbanaa <theodorerodd@gmail.com>
2026-03-26 15:04:55 -0500

    Added movement methods to my player class

--- other.py
+++ other.py
@@ -1,9 +1,21 @@
-class player():
+class Player():
     def __init__(self,name):
         self.name = name
         self.hp = 100
         self.attack = 10
         self.alive = True
+    
+    def move_forward(self):
+        print(f"{self.name} moves 1 step forward")
+
+    def move_backward(self):
+        print(f"{self.name} moves 1 step backward")
+
+    def move_left(self):
+        print(f"{self.name} moves 1 step left")
+
+    def move_right(self):
+        print(f"{self.name} moves 1 step right")
 
     def attack(self):
         print(f"{self.name} swings their weapon dealing {self.attack} damage!")
