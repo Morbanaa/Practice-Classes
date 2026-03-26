14ff9ade389978c73972b0e8541147855b4bb1c4
Morbanaa <theodorerodd@gmail.com>
2026-03-26 15:01:17 -0500

    Added user input to main

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-ECHO is on.
+from other import Player
+
+def main():
+    name = ""
+    while name == "":
+        name = input("Player, please enter your name: ")
+
+    player = Player(name)
+
+    while True:
+        print("Will You:")
+        print("(1) Move Forward")
+        print("(2) Move Backwar")
+        print("(3) Move Left")
+        print("(4) Move Right")
+        print("(5) Attack")
+        print("(6) Heal")
+        choice = input("Choose: ")
+
+
+
+
+if __name__ == "__main__":
+    main()
